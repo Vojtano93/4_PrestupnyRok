7da71fbd3a0220b56b3dbfca0e75d5cc9af86b69
Vojtano93 <189370568+Vojtano93@users.noreply.github.com>
2024-12-09 07:12:48 +0100

    Add files via upload

---
+++ PrestupnyRok.java
@@ -0,0 +1,13 @@
+public class PrestupnyRok {
+    public static void main(String[] args) {
+
+        int a = 11; // rok
+
+        if (a % 4 == 0) {
+            System.out.println("Rok je přestupný");
+        } else {
+            System.out.println("Rok není přestupný");
+
+        }
+    }
+}
